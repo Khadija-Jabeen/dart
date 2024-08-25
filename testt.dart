c58ed873cbebef836072154a11fc7f21164e0087
Khadija-Jabeen <khadijajabeen2004@gmail.com>
2024-08-25 16:59:41 +0500

    first

---
+++ testt.dart
@@ -0,0 +1,6 @@
+void main() {
+  String name = 'khadija';
+  String email = 'kj@gmail.com';
+  String password = '12345';
+  print(!(name == 'khadija' && email == 'kj@gmail.com') || (password == '123') );
+}
